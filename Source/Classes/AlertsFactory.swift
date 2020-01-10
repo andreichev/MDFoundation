fb78dd6d3a0250e616c8625990dbdbb0129f37f1
Andreichev Michael <andreichev.m@mail.ru>
2020-01-11 01:17:02 +0300

    string validator

---
+++ Source/Classes/AlertsFactory.swift
@@ -0,0 +1,60 @@
+//
+//  AlertsFactory.swift
+//  MDFoundation
+//
+//  Created by Михаил Андреичев on 28.11.2019.
+//
+
+open class AlertsFactory {
+    public static func error(
+        title: String,
+        message: String,
+        cancelText: String
+    ) -> UIAlertController {
+        let alert: UIAlertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertController.Style.alert
+        )
+        let cancelAction = UIAlertAction(
+            title: cancelText,
+            style: UIAlertAction.Style.cancel
+        )
+        alert.addAction(cancelAction)
+        return alert
+    }
+
+    public static func accessDeniedWithSettingsLink(
+        title: String,
+        message: String,
+        openSettingsText: String,
+        cancelText: String
+    ) -> UIAlertController {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertController.Style.alert
+        )
+        alert.addAction(UIAlertAction(
+            title: openSettingsText,
+            style: UIAlertAction.Style.default,
+            handler: openSettingsAction(_:)
+        ))
+        alert.addAction(UIAlertAction(
+            title: cancelText,
+            style: UIAlertAction.Style.default,
+            handler: nil
+        ))
+        return alert
+    }
+
+    public static func openSettingsAction(_ action: UIAlertAction) {
+        let app = UIApplication.shared
+        guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+            return
+        }
+        if app.canOpenURL(settingsUrl) {
+            app.open(settingsUrl)
+        }
+    }
+}
---
+++ Source/Classes/StringValidator.swift
@@ -0,0 +1,43 @@
+//
+//  StringValidator.swift
+//  MDFoundation
+//
+//  Created by Михаил Андреичев on 09.01.2020.
+//
+
+/// Пример:
+/// enum AuthValidationType: ValidationType {
+///    case phone
+///    case name
+///
+///    var regularExpression: String {
+///        switch self {
+///        case .phone:
+///            return "^\\(\\d{3}\\)\\s\\d{3}-\\d{2}-\\d{2}$"
+///        case .name:
+///            return "^[А-Яа-я]{2,20}$"
+///        }
+///    }
+/// }
+public protocol ValidationType {
+    var regularExpression: String { get }
+}
+
+/// Пример использования:
+/// if StringValidator.validate(
+///    string: name,
+///    withType: AuthValidationType.name
+/// ) == false { ... }
+public final class StringValidator {
+    /// Validate string with given validation type
+    ///
+    /// - Parameters:
+    ///   - string: string to validate
+    ///   - validationType: validation type
+    /// - Returns: string is valid with given type
+    public static func validate(string: String?, withType validationType: ValidationType) -> Bool {
+        guard let string = string else { return false }
+        let regularExpressionTest = NSPredicate(format: "SELF MATCHES %@", validationType.regularExpression)
+        return regularExpressionTest.evaluate(with: string)
+    }
+}
